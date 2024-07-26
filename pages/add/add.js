b0136bc8cdac26c4f25dea6305c69a93059223c1
zhangbo <mageboer@qq.com>
2024-07-26 22:06:06 +0800

    添加日历功能

--- pages/add/add.js
+++ pages/add/add.js
@@ -5,71 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
+    calendarData: {
+      visible: false,
+      createdDate: 'Todo: 获取当前日期',
+      expirationDate: '点击选择日期',
+      currentCalendar: '',
+    },
+
     defaultListData: [
-      {script: '待办事项', content: "请输入", type: "input"},
-      {script: '创建时间', content: "2024/07/13", type: "text"},
-      {script: '截止日期', content: "图片1", type: "icon"},
-      {script: '距离截止日', content: "提交后自动填写", type: "text"},
-      {script: '是否已完成', content: "括号", type: "checkbox"},
-      {script: '标签', content: "图片2", type: "icon"},
-      {script: '优先级', content: "图片2", type: "icon"},
-      {script: '执行人', content: "图片3", type: "icon"},
-    ]
+      { script: '待办事项', content: "请输入", type: "input" },
+      { script: '创建时间', content: "", type: "calendar" },
+      { script: '截止日期', content: "", type: "calendar" },
+      { script: '距离截止日', content: "提交后自动填写", type: "text" },
+      { script: '是否已完成', content: "括号", type: "checkbox" },
+      { script: '标签', content: "图片2", type: "icon" },
+      { script: '优先级', content: "图片2", type: "icon" }
+    ],
+  },
+
+  onCalendarClick: function (event) {
+    console.log("onCalendarClick");
+    this.setData({
+      'calendarData.visible': true,
+      'calendarData.currentCalendar': event.currentTarget.dataset.source
+    });
+  },
+
+  handleCalendarConfirm: function (event) {
+    const { value } = event.detail;
+    const format = (val) => {
+      const date = new Date(val);
+      return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    };
+
+    if (this.data.calendarData.currentCalendar == '创建时间') {
+      this.setData({
+        'calendarData.createdDate': format(value)
+      })
+      this.updateCalendarData('创建时间', this.data.calendarData.createdDate);
+    } else if (this.data.calendarData.currentCalendar == '截止日期') {
+      this.setData({
+        'calendarData.expirationDate': format(value)
+      });
+      this.updateCalendarData('截止日期', this.data.calendarData.expirationDate);
+    }
+  },
+
+  onCalendarClose: function ({ detail }) {
+    console.log('onCalendarClose');
+    this.setData({ 'calendarData.visible': false });
+    console.log(detail.trigger);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    // 初始化创建时间和截止日期
+    this.updateCalendar();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  updateCalendar: function () {
+    this.updateCalendarData('创建时间', this.data.calendarData.createdDate);
+    this.updateCalendarData('截止日期', this.data.calendarData.expirationDate);
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  updateCalendarData: function (script, newContent) {
+    // 初始化创建时间和截止日期
+    const scriptIndex = this.data.defaultListData.findIndex(item => item.script === script);
+    if (scriptIndex) {
+      this.setData({
+        [`defaultListData[${scriptIndex}].content`]: newContent
+      });
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
