c35b4b0ff4c784123de167403b8ce91d5875b9b4
zhangbo <mageboer@qq.com>
2024-07-29 21:33:09 +0800

    完成详情页

--- pages/add/add.js
+++ pages/add/add.js
@@ -7,7 +7,7 @@ Page({
   data: {
     calendarData: {
       visible: false,
-      createdDate: 'Todo: 获取当前日期',
+      createdDate: '',
       expirationDate: '点击选择日期',
       currentCalendar: '',
     },
@@ -21,29 +21,46 @@ Page({
       { script: '标签', content: "图片2", type: "icon" },
       { script: '优先级', content: "图片2", type: "icon" }
     ],
+
+    formData: {
+      id: '',
+      title: '',
+      createdDate: '',
+      expirationDate: '',
+      isDone: false,
+      tag: '',
+      priority: '',
+    }
   },
 
-  onCalendarClick: function (event) {
+  onCalendarClick(e) {
     console.log("onCalendarClick");
     this.setData({
       'calendarData.visible': true,
-      'calendarData.currentCalendar': event.currentTarget.dataset.source
+      'calendarData.currentCalendar': e.currentTarget.dataset.source
     });
   },
 
-  handleCalendarConfirm: function (event) {
-    const { value } = event.detail;
+  handleInputBlur(e) {
+    const { value } = e.detail;
+    this.setData({
+      'formData.title': value
+    });
+  },
+
+  handleCalendarConfirm(e) {
+    const { value } = e.detail;
     const format = (val) => {
       const date = new Date(val);
       return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
     };
 
-    if (this.data.calendarData.currentCalendar == '创建时间') {
+    if (this.data.calendarData.currentCalendar === '创建时间') {
       this.setData({
-        'calendarData.createdDate': format(value)
+        'calendarData.createdDate': format(value),
       })
       this.updateCalendarData('创建时间', this.data.calendarData.createdDate);
-    } else if (this.data.calendarData.currentCalendar == '截止日期') {
+    } else if (this.data.calendarData.currentCalendar === '截止日期') {
       this.setData({
         'calendarData.expirationDate': format(value)
       });
@@ -51,10 +68,43 @@ Page({
     }
   },
 
-  onCalendarClose: function ({ detail }) {
+  handleIsDone(e) {
+    const { checked } = e.detail;
+    console.log(checked);
+    this.setData({
+      'formData.isDone': checked
+    })
+  },
+
+  onCalendarClose({ detail }) {
     console.log('onCalendarClose');
     this.setData({ 'calendarData.visible': false });
     console.log(detail.trigger);
+  },
+
+  // 点击提交
+  handleSubmit() {
+    try {
+      //debugger;
+      let storedData = wx.getStorageSync('backlogDataList') || [];
+      console.log(storedData);
+      // 将当前表单数据添加到存储数据中
+      this.data.formData.createdDate = this.data.calendarData.createdDate;
+      this.data.formData.expirationDate = this.data.calendarData.expirationDate;
+      console.log(this.data.formData);
+      storedData.push(this.data.formData);
+      // 保存数据到本地存储
+      wx.setStorageSync('backlogDataList', storedData);
+      wx.showToast({
+        title: '保存成功',
+        icon: 'success'
+      });
+    } catch (e) {
+      wx.showToast({
+        title: '保存失败',
+        icon: 'none'
+      });
+    }
   },
 
   /**
@@ -65,12 +115,13 @@ Page({
     this.updateCalendar();
   },
 
-  updateCalendar: function () {
-    this.updateCalendarData('创建时间', this.data.calendarData.createdDate);
+  // utils
+  updateCalendar() {
+    this.updateCalendarData('创建时间', this.getCurrentDate());
     this.updateCalendarData('截止日期', this.data.calendarData.expirationDate);
   },
 
-  updateCalendarData: function (script, newContent) {
+  updateCalendarData(script, newContent) {
     // 初始化创建时间和截止日期
     const scriptIndex = this.data.defaultListData.findIndex(item => item.script === script);
     if (scriptIndex) {
@@ -79,4 +130,9 @@ Page({
       });
     }
   },
-})
+
+  getCurrentDate() {
+    const date = new Date();
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  }
+});
